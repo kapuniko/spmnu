8db99718f37009b86dcdd2262d4efd011ac22833
Kapuniko <kapuniko@gmail.com>
2024-04-30 21:27:50 +0500

    Бейджики в списке объектов

---
+++ app/MoonShine/Fields/RowFields.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Fields;
+
+use MoonShine\Fields\StackFields;
+use Closure;
+
+class RowFields extends StackFields
+{
+    protected string $view = 'admin.fields.row-fields';
+
+}
